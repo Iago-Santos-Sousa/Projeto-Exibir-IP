3c8607a9fb432fa9e423b229d4aa7fad37b30412
Iago-Santos-Sousa <iago.santos.sousa@mail.com>
2023-02-15 01:13:52 -0300

    Exibir-IP

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+   const [dado, setDado] = useState({ endereco: "" });
+   const [classe, setClasse] = useState(true);
+
+   useEffect(() => {
+      async function getApi() {
+         const response = await fetch("https://ip4.seeip.org/json");
+         const data = await response.json();
+
+         const endereco = data.ip;
+         setDado({ ...dado, endereco });
+      }
+
+      getApi();
+   }, []);
+
+   function showIp() {
+      setClasse(false);
+   }
+
+   return (
+      <div className="App">
+         <button onClick={showIp}>Ver IP</button>
+         <p className={classe ? "hidden" : "show"}>{dado.endereco}</p>
+      </div>
+   );
+}
+
+export default App;
