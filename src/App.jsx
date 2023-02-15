0a95003cfe472226c7f2f82048548fa78603b1ad
Iago-Santos-Sousa <iago.santos.sousa@mail.com>
2023-02-15 14:31:39 -0300

    Exibir-IP

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import Loading from "./Loading";
 
 function App() {
    const [dado, setDado] = useState({ endereco: "" });
    const [classe, setClasse] = useState(true);
+   const [loading, setLoading] = useState(false);
 
    async function getApi() {
       const response = await fetch("https://ip4.seeip.org/json");
       const data = await response.json();
-
       const endereco = data.ip;
       setDado({ ...dado, endereco });
+      setLoading(true);
    }
 
    useEffect(() => {
-      getApi();
+      setTimeout(() => {
+         getApi();
+      }, 3000);
    }, []);
-
-   function showIp() {
-      setClasse(false);
-   }
 
    return (
       <div className="App">
-         <button onClick={showIp}>Ver IP</button>
-         <p className={classe ? "hidden" : "show"}>{dado.endereco}</p>
+         <button onClick={() => setClasse(false)}>Ver IP</button>
+
+         <div className={classe ? "hidden" : "show result"}>
+            {loading ? dado.endereco : <Loading />}
+         </div>
       </div>
    );
 }
---
+++ src/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+   return (
+      <div id="loader">
+         <i className="fa-solid fa-spinner"></i>
+      </div>
+   );
+};
+
+export default Loading;
